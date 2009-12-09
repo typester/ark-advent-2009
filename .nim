a088833ede2aeea50a55a682b9fe5ba35c19c14e
Daisuke Murase <typester@cpan.org>
2009-12-09 20:53:52 +0900

    day 09

--- .nim
+++ .nim
@@ -6,6 +6,17 @@ site_dir: ./site
 plugins:
   - module: Meta
   - module: Markdown
+
+  - module: EmacsColor
+    config:
+      filter: '(<(?:h|div|p|html|span|table|tr|td))'
+      mode: html
+
+  - module: EmacsColor
+    config:
+      filter: '(?:my \(?[\$\@\%]|use Ark|use [0-9a-zA-Z_:]+;|__PACKAGE__|\$[\w_]+->)'
+      mode: cperl
+
   - module: AutoIndex
   - module: AutoIndex
     config:
